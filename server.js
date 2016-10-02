13d4cb0e960035cc25bd5e7b3192caf47dab840d
apurva21 <maupadhyay@gmail.com>
2016-10-02 15:35:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,12 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/article-one', function (req, res) {
     res.send('Artcle one is requested here');
-});
-app.get('/article-two', function (req, res) {
-    res.send('Artcle two is requested here');
-});
-app.get('/article-three', function (req, res) {
-    res.send('Artcle three is requested here');
+
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
